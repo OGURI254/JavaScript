26ee5e401878542e11a51180208a4033d00a1397
JOSEPH OGURI OGEGA <92936911+OGURI254@users.noreply.github.com>
2023-02-22 09:45:24 -0800

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,78 @@
+//Q1
+
+
+const name = "Joseph";
+const age = 21;
+
+const message = `My name is ${name} and I am ${age} years old.`;
+
+console.log(message);
+
+//Q2
+
+if (fruits.length > 3) {
+    console.log("You have a lot of fruits!");
+  } else {
+    console.log("You need to buy more fruits!");
+  }
+  
+
+  //Q3
+  const mtu = {
+    name: "Josep Oguri",
+    age: 21,
+    city: "Juja City",
+    hobbies: ["Design", "code", "learning new things"]
+  };
+  
+
+//Q4
+  const mtu = {
+    name: 'joseph Oguri',
+    age: 21,
+    city: 'Juja city'
+  };
+  
+  console.log(mtu.age);
+  
+//Q5
+  const mtu = {
+    name: 'joseph Oguri',
+    age: 21,
+    city: 'Juja city'
+  };
+  
+  mtu['occupation'] = 'Product design Engineer at Vstech';
+  
+  console.log(mtu);
+  
+//Q6
+
+  const fruits = [
+    { name: "apple", color: "red", taste: "sweet" },
+    { name: "banana", color: "yellow", taste: "sweet" },
+    { name: "orange", color: "orange", taste: "sour" }
+  ];
+
+  //Q7
+
+  const fruits = [
+    { name: "apple", color: "red", taste: "sweet" },
+    { name: "banana", color: "yellow", taste: "sweet" },
+    { name: "orange", color: "orange", taste: "sour" }
+  ];
+  
+  for (const fruit of fruits) {
+    console.log(`The ${fruit.name} is ${fruit.color} and tastes ${fruit.taste}.`);
+  }
+  
+ // Q8
+
+ function addNumbers(num1, num2) {
+    return num1 + num2;
+  }
+
+const result = addNumbers(5, 7);
+  console.log(result);
+  
+  
